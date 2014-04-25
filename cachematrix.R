d77b1677cb85bb0579cfdddeaf543828950ea319
Casey Tsui <casey.tsui@gmail.com>
2014-04-24 21:40:58 -0700

    Commit assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+# R Programming Assignment 2
+# By Casey Tsui
+# Format: R
+# Last updated: 4/24/14
 
-## Write a short comment describing this function
+
+# Author's Note: This code stores a matrix in cache and later, if the same 
+# matrix is called, the cacheSolve function checks to see if it is stored in
+# cache and returns the cached value if identical. If it isn't, then it 
+# returns the inverse matrix and stores the new matrix into cache
+
+
+# FUNCTIONS ###################################################################
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        # This function creates a special "matrix" object that can cache its
+        # inverse
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinvmat <- function(solve) m <<- solve
+        getinvmat <- function() m
+        list(set = set, get = get,
+             setinvmat = setinvmat,
+             getinvmat = getinvmat)
 }
 
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        # Return a matrix that is the inverse of x using a special 'matrix'
+        # object from the makeCacheMatrix function as input
+        m <- x$getinvmat()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinvmat(m)
+        m
 }
+
+
