97f6d2da385f00e0b13bf3cd2b5c2930167f95bc
Casey Tsui <casey.tsui@gmail.com>
2014-04-26 00:03:17 -0700

    Final commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,7 @@
 # R Programming Assignment 2
 # By Casey Tsui
-# Format: R
-# Last updated: 4/24/14
+# Format: R version 3.0.3 (2014-03-06)R
+# Last updated: 4/25/14
 
 
 # Author's Note: This code stores a matrix in cache and later, if the same 
@@ -12,36 +12,45 @@
 
 # FUNCTIONS ###################################################################
 
-
+## This function creates a list object that can cache the inverse of the
+## matrix that is used as the x argument
 makeCacheMatrix <- function(x = matrix()) {
-        # This function creates a special "matrix" object that can cache its
-        # inverse
         m <- NULL
         set <- function(y) {
                 x <<- y
                 m <<- NULL
         }
-        get <- function() x
-        setinvmat <- function(solve) m <<- solve
-        getinvmat <- function() m
+        get <- function() {
+                x
+        }
+        setinvmat <- function(invmat) {
+                m <<- invmat
+        }
+        getinvmat <- function() {
+                m
+        }
         list(set = set, get = get,
              setinvmat = setinvmat,
              getinvmat = getinvmat)
 }
 
-
+## Using the returned list object of the makeCacheMatrix function, this
+## return an inverse of the original matrix that was used as input for
+## the makeCacheMatrix function
 cacheSolve <- function(x, ...) {
-        # Return a matrix that is the inverse of x using a special 'matrix'
-        # object from the makeCacheMatrix function as input
+        # Pull up the cached value of the inverse matrix, if it exists
         m <- x$getinvmat()
+        # If a cache exists, then the function returns a message and cache
         if(!is.null(m)) {
                 message("getting cached data")
                 return(m)
         }
+        # If a cache does not exist, then the function calculates the inverse,
+        # then stores the inverse matrix into x's cache
         data <- x$get()
-        m <- solve(data, ...)
-        x$setinvmat(m)
-        m
+        m <- solve(data, ...)  # Calculate inverse matrix
+        x$setinvmat(m)         # Save result to x's cache
+        m                      # Return m
 }
 
 
